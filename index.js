09e12613105cf40df85411cfb91c35479f1524ff
rrequero <rareq1987@gmail.com>
2016-07-20 23:59:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const apiRouter = require('./lib/auth.router');
+const authService = require('./lib/auth.service');
+const passport = require('koa-passport');
+const debug = require('debug')('oauth-plugin');
+const mongoose = require('mongoose');
+const jwt = require('koa-jwt');
+const views = require('koa-views');
+
+function init() {
+
+}
+
+function middleware(app, plugin, generalConfig) {
+    mongoose.connect(`${generalConfig.mongoUri}/users`);
+    debug('Loading oauth-plugin');
+
+    app.use(views(`${__dirname}/lib/views`, {
+        map: {
+            html: 'ejs',
+        },
+    }));
+    authService(plugin);
+    app.use(passport.initialize());
+    app.use(passport.session());
+    if (plugin.config.jwt.active) {
+        app.use(jwt({
+            secret: plugin.config.jwt.secret,
+            passthrough: plugin.config.jwt.passthrough,
+        }));
+    }
+    app.use(apiRouter(plugin).middleware());
+
+}
+
+
+module.exports = {
+    middleware,
+    init,
+};
